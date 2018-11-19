08d41dd76a8d1567596f8f843ee082366bd3a8c0
dayudodo <dayudodo@gmail.com>
2018-11-19 18:05:22 +0800

    add checkAnswer
    无法播放声音，添加了判断模块

--- pages/check/check.js
+++ pages/check/check.js
@@ -1,8 +1,12 @@
 //check.js
+var question = 'A';
+function checkRight (q, answer){
+  return q == answer
+}
 
 Page({
   data: {
-    mp3Source: '/sounds/a.mp3'
+    mp3Source: '../sounds/a.mp3'
   },
   //事件处理函数
   bindViewTap: function () {
@@ -18,10 +22,17 @@ Page({
 
   },
   tapChar:function(event){
-    console.log(event.currentTarget.dataset.char)
-  },
-  checkEqual:function(){
+    console.log(question, event.currentTarget.dataset.char)
+    let answer = event.currentTarget.dataset.char
+    if(checkRight(question, answer)){
+      console.log('right')
+      //下一个问题
+    }else{
+      //给出错误提示
+      console.log('wrong')
+    }
 
   },
+
 
 })
