0a08277d0e96896c62b3189b40cc45efbea70e15
dayudodo <dayudodo@gmail.com>
2018-11-21 18:10:21 +0800

    测试基本完成

--- pages/check/check.js
+++ pages/check/check.js
@@ -1,21 +1,58 @@
 //check.js
 const mainhttp = "https://www.gsenglish.cn/zhimu/"
-var question = 'A';
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+//问题就来自于26个字母，自然也是大写的
+
+//变大写，简单点儿
+function Upper(c) {
+  return c.toUpperCase()
+}
+
+//随机从数组中取出一个数
+var sample = function(arr) {
+  let len = arr.length
+  let index;
+  do {
+    index = parseInt(Math.random() * len)
+  } while (arr[index] == currentQ)
+  return arr[index]
+
+};
+var shuffle = function(arr) {
+  return arr.sort(function() {
+    return .5 - Math.random();
+  });
+}
+
 
 function checkRight(q, answer) {
-  return q == answer
+  return q.toLowerCase() == answer.toLowerCase()
 }
-function getQuestionSound(q){
+//获取到字母的声音
+function getCharSound(q) {
   return `${mainhttp}${q.toLowerCase()}.mp3`
+}
+
+var qIndex = 0
+var currentQ = alphabet[qIndex]
+
+function makeQuestion() {
+  if (currentQ == 'z') {
+    //todo 成功完成测试
+  } else {
+
+  }
 }
 
 Page({
   data: {
     mp3Source: mainhttp + "a.mp3",
+    options: []
   },
   onReady: function(e) {
     // 使用 wx.createAudioContext 获取 audio 上下文 context
     this.audioCtx = wx.createAudioContext('myAudio')
+    this.changeOptions()
   },
   //事件处理函数
   bindViewTap: function() {
@@ -24,9 +61,10 @@ Page({
     })
   },
 
-  listenMe: function() {
+  playQuestion: function() {
     // console.log('a')
-    this.audioCtx.setSrc(getQuestionSound(question))
+    this.audioCtx.pause()
+    this.audioCtx.setSrc(getCharSound(currentQ))
     this.audioCtx.play()
   },
   playRight: function() {
@@ -39,20 +77,39 @@ Page({
     this.audioCtx.play()
   },
   tapChar: function(event) {
-    console.log(question, event.currentTarget.dataset.char)
+    console.log(currentQ, event.currentTarget.dataset.char)
     let answer = event.currentTarget.dataset.char
-    if (checkRight(question, answer)) {
+    if (checkRight(currentQ, answer)) {
       console.log('bang')
       this.playRight()
-
       //下一个问题
+      this.nextQuestion()
     } else {
       //给出错误提示
       console.log('wrong')
       this.playWrong()
     }
+  },
+  changeOptions() {
+    let options = shuffle([Upper(currentQ), Upper(sample(alphabet))])
+    console.log("options: ", options)
+    this.setData({
+      options: options
+    })
 
   },
+  nextQuestion: function() {
+    if (qIndex + 1 == alphabet.length) {
+      //最后一个测试完成，给予奖励！
+
+    } else {
+      //下一个测试项目
+      currentQ = alphabet[++qIndex]
+      this.changeOptions()
+      //取出字母数组中的下一个
+      this.playQuestion()
+    }
+  }
 
 
 })
