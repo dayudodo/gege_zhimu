211ab19be12247446e42fc6cfecc752b4e15ca4f
dayudodo <dayudodo@gmail.com>
2018-12-27 17:50:06 +0800

    改成按钮进行翻页，索引有问题

--- pages/study/study.js
+++ pages/study/study.js
@@ -3,11 +3,16 @@ const util = require('../../utils/util.js')
 const zhimu_group = ['abcd', 'efgh', 'ijkl', 'mnop', 'qrst', 'uvwx', 'yz']
 const colors = ['red', 'orange', 'green', 'black', 'blue', 'brown', 'cyan']
 // const font_groups = ['黑体','宋体']
-const plus = (i, arr) => {
+var plus = function(i, arr) {
   var result;
   i + 1 >= arr.length ? result = 0 : result = i + 1
   return result;
-}
+};
+var minus = function(i, arr) {
+  var result;
+  i - 1 < 0 ? result = arr.length -1 : result = i - 1
+  return result
+};
 
 var g_height; //当前窗口的高度  
 //字母颜色索引，每次点击的时候颜色都不同！
@@ -27,55 +32,73 @@ Page({
   },
   onLoad: function() {
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         g_height = res.windowHeight
         console.log(g_height)
       },
     })
     this.setData({
-      colorGroup: this.getColorGroup(),
-      currentCharGroup: this.getCurrentCharGroup(),
+      colorGroup: this.getColorGroup("plus"),
+      currentCharGroup: this.getCurrentCharGroup("plus"),
     })
 
   },
-  onShow:function(){
+  onShow: function() {
     // console.log(this.data)
   },
-  onReady: function(e) {
-  },
-  onReachBottom:function(){
-    // console.log('onReachBottom')
-    this.nextGroup()
-  },
+  onReady: function(e) {},
+  // onReachBottom:function(){
+  //   // console.log('onReachBottom')
+  //   this.nextGroup()
+  // },
   // pullUpNext:function(){
   //   console.log('pullUpNext')
   // },
-  getColorGroup: function() {
+  tapPrev: function(){
+    this.prevGroup()
+  },
+  tapNext: function() {
+    this.nextGroup()
+  },
+  getColorGroup: function(how) {
     //获取到颜色组供自定义组件使用！
     let group = []
     for (var i in current_group) {
-      let color_name=colors[color_index]
-      color_index = plus(color_index, colors)
+      let color_name = colors[color_index]
+      if (how == 'plus') {
+        color_index = plus(color_index, colors)
+      } else if(how == "minus"){
+        color_index = minus(color_index, colors)
+      }
       group.push(color_name)
     }
-    console.log("group:",group)
+    console.log("color_index:", color_index)
     return group;
   },
-  getCurrentCharGroup:function(){
-    let group=[]
+  getCurrentCharGroup: function(how) {
+    let group = []
     //当前字母组改变
     current_group = Array.from(zhimu_group[group_index])
-    group=current_group.map(char=>char.toUpperCase())
+    group = current_group.map(char => char.toUpperCase())
     //先显示出来，因为onLoad也调用了此方法！更新索引为下一次做准备
-    group_index = plus(group_index, zhimu_group)
+    if(how == "plus"){
+      group_index = plus(group_index, zhimu_group)
+    } else if (how == "minus"){
+      group_index = minus(group_index, zhimu_group)
+    }
+    console.log("group_index", group_index)
     return group;
   },
-  nextGroup: function() {
-
-
+  prevGroup: function () {
     this.setData({
-      colorGroup: this.getColorGroup(),
-      currentCharGroup: this.getCurrentCharGroup(),
+      colorGroup: this.getColorGroup("minus"),
+      currentCharGroup: this.getCurrentCharGroup("minus"),
+    })
+  },
+  nextGroup: function() {
+    this.setData({
+      colorGroup: this.getColorGroup("plus"),
+      currentCharGroup: this.getCurrentCharGroup("plus"),
     })
   },
   changeFont: function() {
