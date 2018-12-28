c10f360c11bd2ed71018fb20ff16b9e5d88f8e31
dayudodo <dayudodo@gmail.com>
2018-12-28 14:44:12 +0800

    添加circleGroup类用来处理循环取用数组

--- pages/study/study.js
+++ pages/study/study.js
@@ -10,19 +10,45 @@ var plus = function(i, arr) {
 };
 var minus = function(i, arr) {
   var result;
-  i - 1 < 0 ? result = arr.length -1 : result = i - 1
+  i - 1 < 0 ? result = arr.length - 1 : result = i - 1
   return result
 };
 
+class CircleGroup {
+  constructor(arr) {
+    this.arr = arr
+    this.index = 0
+  }
+  current() {
+    return this.arr[this.index]
+  }
+  prev() {
+    this.index = (this.index - 1 < 0) ? this.arr.length - 1 : this.index - 1
+    return this.current()
+  }
+  next(count) {
+    if (count) {
+      let group = []
+      group.push(this.current())
+      for (let index = 0; index < 3; index++) {
+        group.push(this.next())
+      }
+      return group
+    } else {
+      this.index = (this.index + 1 >= this.arr.length) ? 0 : this.index + 1
+      return this.current()
+    }
+  }
+
+}
+
+var colorGroup = new CircleGroup(colors)
+var charGroup = new CircleGroup(zhimu_group.map(c=>c.toUpperCase()))
+// console.log(colorGroup.current(), charGroup.current())
 var g_height; //当前窗口的高度  
-//字母颜色索引，每次点击的时候颜色都不同！
-var color_index = 0;
-//字母索引，需要显示哪个位置的索引
-var char_index = 0;
-//第几组字母
-var group_index = 0;
-//当前字母组
-var current_group = zhimu_group[group_index]
+
+
+
 
 
 Page({
@@ -38,8 +64,8 @@ Page({
       },
     })
     this.setData({
-      colorGroup: this.getColorGroup("plus"),
-      currentCharGroup: this.getCurrentCharGroup("plus"),
+      colorGroup: colorGroup.next(4),
+      currentCharGroup: charGroup.current(),
     })
 
   },
@@ -54,7 +80,7 @@ Page({
   // pullUpNext:function(){
   //   console.log('pullUpNext')
   // },
-  tapPrev: function(){
+  tapPrev: function() {
     this.prevGroup()
   },
   tapNext: function() {
@@ -62,43 +88,21 @@ Page({
   },
   getColorGroup: function(how) {
     //获取到颜色组供自定义组件使用！
-    let group = []
-    for (var i in current_group) {
-      let color_name = colors[color_index]
-      if (how == 'plus') {
-        color_index = plus(color_index, colors)
-      } else if(how == "minus"){
-        color_index = minus(color_index, colors)
-      }
-      group.push(color_name)
-    }
-    console.log("color_index:", color_index)
-    return group;
+
   },
   getCurrentCharGroup: function(how) {
-    let group = []
-    //当前字母组改变
-    current_group = Array.from(zhimu_group[group_index])
-    group = current_group.map(char => char.toUpperCase())
-    //先显示出来，因为onLoad也调用了此方法！更新索引为下一次做准备
-    if(how == "plus"){
-      group_index = plus(group_index, zhimu_group)
-    } else if (how == "minus"){
-      group_index = minus(group_index, zhimu_group)
-    }
-    console.log("group_index", group_index)
-    return group;
+
   },
-  prevGroup: function () {
+  prevGroup: function() {
     this.setData({
-      colorGroup: this.getColorGroup("minus"),
-      currentCharGroup: this.getCurrentCharGroup("minus"),
+      colorGroup: colorGroup.next(4),
+      currentCharGroup: charGroup.prev(),
     })
   },
   nextGroup: function() {
     this.setData({
-      colorGroup: this.getColorGroup("plus"),
-      currentCharGroup: this.getCurrentCharGroup("plus"),
+      colorGroup: colorGroup.next(4),
+      currentCharGroup: charGroup.next(),
     })
   },
   changeFont: function() {
